8d5b2a67323a48ca4a3b033be4d4b9aea62da286
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-07-04 12:28:04 -0500

    got my page running on a new github project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [employees, setEmployees] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [sortAsc, setSortAsc] = useState(true)
+  
+  
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=75")
+    .then(res=>res.json())
+    .then(data =>{
+      console.log(data.results)
+      setEmployees(data.results)
+
+    })
+  },[]) 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value)}}type="text"/>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>email</th>
+            <th onClick={(event)=>{setSortAsc(!sortAsc)}}>age</th>
+            <th>picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees
+          .filter((employee)=>{
+            return employee.email.includes(searchTerm)
+
+          })
+          .sort((employeeA, employeeB)=>{
+            //added a ternary statement to sort employee age
+            return (employeeA.dob.age - employeeB.dob.age) * (sortAsc?1:-1)
+
+            
+          })
+          .map((employee, index)=>{
+            return <tr>
+              <td>{employee.name.first} {employee.name.last}</td>
+              <td><a href={"mailto:" + employee.email}>{employee.email}</a></td>
+              <td>{employee.dob.age}</td>
+              <td><img src= {employee.picture.thumbnail}/></td>
+            </tr>
+
+          })}
+        </tbody>
+
+      </table>
     </div>
   );
 }
